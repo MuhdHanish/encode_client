2ae432bae6c8b5e2eea15c2a984a532b1d38771d
MuhdHanish <muhammedhanish11@gmail.com>
2023-08-21 10:59:31 +0530

    add filtering in the progress page also

--- src/components/Student/StudentProgress/StudentProgress.tsx
+++ src/components/Student/StudentProgress/StudentProgress.tsx
@@ -8,6 +8,7 @@ import SideCard from './SideCard/SideCard';
 import HeadCard from './HeadCard/HeadCard';
 
 const StudentProgress: React.FC = () => {
+  
   const navigate = useNavigate();
   const [courses, setCourses] = useState<Course[] | []>([]);
   const fetchDatas = useCallback(() => {
@@ -15,19 +16,52 @@ const StudentProgress: React.FC = () => {
   }, []);
   useEffect(() => {
     fetchDatas();
-  }, [fetchDatas])
+  }, [fetchDatas]);
+  const [selectedOption, setSelectedOption] = useState<string>("option1");
+  const [selectedLevel, setSelectedLevel] = useState<string>("option1");
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [filteredCourseList, setFilteredCourseList] = useState<Course[] | []>(
+    []
+  );
+  useEffect(() => {
+    const filteredList = courses.filter((course) => {
+      const coursenameMatch = course?.coursename
+        ?.toLowerCase()
+        .includes(searchQuery.toLowerCase());
+      const descriptionMatch = course?.description
+        ?.toLowerCase()
+        .includes(searchQuery.toLowerCase());
+      const languageMatch = course?.language
+        ?.toLowerCase()
+        .includes(searchQuery.toLowerCase());
+      const courseTypeMatch =
+        selectedOption === "option1" ||
+        (selectedOption === "option2" && !course.isPaid) ||
+        (selectedOption === "option3" && course.isPaid);
 
+      const levelMatch =
+        selectedLevel === "option1" ||
+        (selectedLevel === "option2" && course.level === "Beginner") ||
+        (selectedLevel === "option3" && course.level === "Intermediate") ||
+        (selectedLevel === "option4" && course.level === "Expert");
+
+      return (
+        (coursenameMatch || descriptionMatch || languageMatch) &&
+        courseTypeMatch &&
+        levelMatch
+      );
+    });
+    setFilteredCourseList(filteredList);
+  }, [searchQuery, courses, selectedOption, selectedLevel]);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const postPerPage = 3;
+  const postPerPage = 6;
   const lastPostIndex = currentPage * postPerPage;
   const firstPostIndex = lastPostIndex - postPerPage;
   const [currentPosts, setCurrentPosts] = useState<Course[] | []>([]);
-
   const handleRemoveStudent = (courseId: string) => { removeStudent(courseId).then((res) => { if (res) { fetchDatas() } }).catch(err => console.error(err)) };
-
   useEffect(() => {
-    setCurrentPosts(courses.slice(firstPostIndex, lastPostIndex));
-  }, [courses, firstPostIndex, lastPostIndex]);
+    setCurrentPosts(filteredCourseList.slice(firstPostIndex, lastPostIndex));
+  }, [filteredCourseList, firstPostIndex, lastPostIndex]);
   
   return (
     <div className="bg-white flex flex-col w-full h-full p-5 gap-8 overflow-x-hidden ">
@@ -50,28 +84,78 @@ const StudentProgress: React.FC = () => {
       </div>
       <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-10 ">
         {courses?.slice(0, 4)?.map((course, idx) => (
-         <HeadCard course={course} key={idx} handleRemoveStudent={handleRemoveStudent}/>
+          <HeadCard
+            course={course}
+            key={idx}
+            handleRemoveStudent={handleRemoveStudent}
+          />
         ))}
       </div>
-      <div className="flex w-full h-fit   justify-center">
-        <div className="  p-5 gap-5 grid sm:grid-cols-3">
-          {currentPosts?.map((course, idx) => (
-            <>
-              <SideCard course={course} key={idx} handleRemoveStudent={handleRemoveStudent} />
-            </>
-          ))}
+      <div className="flex w-full font-medium text-[15px] items-center  gap-3 flex-col md:flex-row ">
+        <div className="flex w-full h-fit gap-3">
+          <div className="flex w-full h-fit bg-white ">
+            <select
+              className="appearance-none w-full bg-white border text-[14px] border-gray-300 rounded py-2 px-4  text-gray-700 leading-tight focus:outline-none focus:border-primary"
+              name="userStatus"
+              id="userStatus"
+              value={selectedOption}
+              onChange={(e) => setSelectedOption(e.target.value)}
+            >
+              <option value="option1">Free + Paid Courses</option>
+              <option value="option2">Free Courses</option>
+              <option value="option3">Paid Courses</option>
+            </select>
+          </div>
+          <div className="flex w-full h-fit bg-white ">
+            <select
+              className="appearance-none w-full bg-white border text-[14px] border-gray-300 rounded py-2 px-4  text-gray-700 leading-tight focus:outline-none focus:border-primary"
+              name="userStatus"
+              id="userStatus"
+              value={selectedLevel}
+              onChange={(e) => setSelectedLevel(e.target.value)}
+            >
+              <option value="option1">Selected Level</option>
+              <option value="option2">Beginner</option>
+              <option value="option3">Intermediate</option>
+              <option value="option4">Expert</option>
+            </select>
+          </div>
+        </div>
+        <div className="flex w-full h-fit bg-white ">
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            placeholder="search"
+            className="appearance-none bg-white border w-full border-gray-300  text-[14px] rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-primary"
+          />
         </div>
       </div>
-          {courses.length > postPerPage && (
-            <div className="flex w-full  justify-end items-end ">
-              <Pagination
-                postsPerPage={postPerPage}
-                totalPosts={courses?.length}
-                setCurrentPage={setCurrentPage}
-                currentPage={currentPage}
+      <div className="flex w-full h-fit   justify-center">
+        <div className=" p-5 gap-5 grid sm:grid-cols-3">
+          {currentPosts.length > 0 ? (
+            currentPosts?.map((course, idx) => (
+              <>
+                <SideCard
+                  course={course}
+                  key={idx}
+                  handleRemoveStudent={handleRemoveStudent}
                 />
-          </div>
-            )}
+              </>
+            ))
+          ) : (<span>No course found !</span>)}
+        </div>
+      </div>
+      {filteredCourseList.length > postPerPage && (
+        <div className="flex w-full  justify-end items-end ">
+          <Pagination
+            postsPerPage={postPerPage}
+            totalPosts={filteredCourseList?.length}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+          />
+        </div>
+      )}
     </div>
   );
 }
